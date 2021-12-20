06ec93629a66420be1922ae74458b4c4a8a973e2
Durgesh Chaudhary <48durgesh.kumar@gmail.com>
2021-12-20 23:19:35 +0530

    already started/stopped case added

--- exercises/car-game.py
+++ exercises/car-game.py
@@ -18,15 +18,28 @@ start_msg = "Car started...Let's go!!!"
 stop_msg = "Car stopped."
 error_msg = "Sorry, I don't understand..."
 
+started = False
+
 while 2 > 1:
   command = input('>').lower()
 
   if command == 'help':
     print(help_msg)
+  
   elif command == 'start':
-    print(start_msg)
+    if started:
+      print('Already running!')
+    else:
+      started = True
+      print(start_msg)
+  
   elif command == 'stop':
-    print(stop_msg)
+    if not started:
+      print("Already stopped!")
+    else:
+      started = False
+      print(stop_msg)
+  
   elif command == 'quit':
     break
   else:
